87e9b50e66d1cb444f1621084adda34cbee51e4e
Jamie Cansdale <jcansdale@gmail.com>
2020-01-22 12:58:11 +0000

    Add .NET Core tool project

---
+++ src/GprTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GprTool
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, GitHub Package Registry!");
+        }
+    }
+}
