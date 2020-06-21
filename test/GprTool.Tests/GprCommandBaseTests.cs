47d7ade471c5fc7e963a22abfdcb4b0d72b806f5
Jamie Cansdale <jcansdale@github.com>
2020-06-21 13:11:43 +0100

    Add support for GITHUB_TOKEN

--- test/GprTool.Tests/GprCommandBaseTests.cs
+++ test/GprTool.Tests/GprCommandBaseTests.cs
@@ -10,11 +10,14 @@ public static class GprCommandBaseTests
 {
     public class TheGetAccessTokenMethod
     {
-        [TestCase("AccessToken", "AccessToken")]
-        public void GetAccessToken(string accessToken, string expectToken)
+        [TestCase("AccessToken", null, "AccessToken")]
+        [TestCase(null, "GitHubToken", "GitHubToken")]
+        [TestCase("AccessToken", "GitHubToken", "AccessToken")]
+        public void GetAccessToken(string accessToken, string githubToken, string expectToken)
         {
             var target = Substitute.For<GprCommandBase>();
             target.AccessToken = accessToken;
+            Environment.SetEnvironmentVariable("GITHUB_TOKEN", githubToken);
 
             var token = target.GetAccessToken();
 
