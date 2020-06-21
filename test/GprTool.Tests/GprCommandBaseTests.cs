d88c1b99700c109ea94c17abdc3fd92c58f14809
Jamie Cansdale <jcansdale@github.com>
2020-06-21 13:15:38 +0100

    Add test for READ_PACKAGES_TOKEN

--- test/GprTool.Tests/GprCommandBaseTests.cs
+++ test/GprTool.Tests/GprCommandBaseTests.cs
@@ -10,14 +10,17 @@ public static class GprCommandBaseTests
 {
     public class TheGetAccessTokenMethod
     {
-        [TestCase("AccessToken", null, "AccessToken")]
-        [TestCase(null, "GitHubToken", "GitHubToken")]
-        [TestCase("AccessToken", "GitHubToken", "AccessToken")]
-        public void GetAccessToken(string accessToken, string githubToken, string expectToken)
+        [TestCase("AccessToken", null, null, "AccessToken")]
+        [TestCase(null, "GitHubToken", null, "GitHubToken")]
+        [TestCase("AccessToken", "GitHubToken", null, "AccessToken")]
+        [TestCase(null, null, "ReadPackagesToken", "ReadPackagesToken")]
+        [TestCase(null, "GitHubToken", "ReadPackagesToken", "GitHubToken")]
+        public void GetAccessToken(string accessToken, string githubToken, string readToken, string expectToken)
         {
             var target = Substitute.For<GprCommandBase>();
             target.AccessToken = accessToken;
             Environment.SetEnvironmentVariable("GITHUB_TOKEN", githubToken);
+            Environment.SetEnvironmentVariable("READ_PACKAGES_TOKEN", readToken);
 
             var token = target.GetAccessToken();
 
